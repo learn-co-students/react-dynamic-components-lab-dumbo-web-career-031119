cfcfca203c850b2a8600aa03011810fbe35994c5
github username <itsDario1@gmail.com>
2019-05-12 09:34:36 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,27 @@
 // add Comment component here
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+    render() {
+        return (
+            <div className='comment'>
+                {this.props.commentText}
+            </div>
+        )
+    }
+}
+
+
+// import React, { Component } from 'react';
+
+// export default class ColorBox extends Component {
+
+//   render() {
+//     return (
+//       <div className="color-box" style={{opacity: 2}}>
+//         {/* your conditional code here! */}
+//       </div>
+//     )
+//   }
+
+// }
